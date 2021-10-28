25f9e0bf88841c2fc2665d256f7afa1dcc38d89d
Octavian Torcea <octavian.torcea@YAHOO.COM>
2021-10-29 00:12:53 +0300

    Update Constants.java

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -2,13 +2,10 @@ package common;
 
 /**
  * The class contains the minimum of constants needed.
- *
- * You can define your own constants here or create separate files.
  */
 public final class Constants {
     private Constants() {
     }
-
     public static final String DATABASE = "database";
     public static final String ACTORS = "actors";
     public static final String MOVIES = "movies";
@@ -54,27 +51,6 @@ public final class Constants {
     public static final String QUERY = "query";
     public static final String COMMAND = "command";
     public static final String RECOMMENDATION = "recommendation";
-
-    // checker constants
-    public static final String ID_STRING = "id";
-    public static final String LARGE = "large";
-    public static final String NO_VALUES = "recommendation_no_values";
-    public static final String TESTS_PATH = "test_db/test_files/";
-    public static final String OUT_PATH = "result/out_";
-    public static final String REF_PATH = "ref";
-    public static final String RESULT_PATH = "result";
-    public static final String JAR_PATH = "src/checker/checkstyle-8.36.2-all.jar";
-    public static final String XML_PATH = "src/checker/poo_checks.xml";
-    public static final String CHECKSTYLE_FILE = "checkstyle.txt";
-    public static final String OUT_FILE = "out.txt";
-    public static final int MIN_LINES = 2;
-    public static final int NUM_CHECK_INFO = 3;
-    public static final int MIN_CHECKSTYLE_ERR = 20;
-    public static final int SINGLE_TEST = 2;
-    public static final int LARGE_TEST = 3;
-    public static final int MAX_LENGTH = 50;
-
-    // add any constants you think you may use
     public static final String BASIC = "BASIC";
     public static final String PREMIUM = "PREMIUM";
     public static final String FAVORITE = "favorite";
